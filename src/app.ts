910944038ee66571d36a633fbf450d85a4cc2652
Leticia Bora <leticia.bora@capitara.ai>
2025-02-19 20:09:02 -0300

    add database

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+import express, { Express, Request, Response } from 'express';
+import sequelize from '@config/database';
+
 const app: Express = express();
 const port = process.env.PORT || 4000;
+
+const startDatabase = async () => await sequelize.sync();
+
+startDatabase();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,35 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD,
+  `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`,
   {
-    host: process.env.DB_HOST,
     dialect: 'mysql',
-  },
+  }
 );
+
+const startDatabase = async () => {
+  try {
+    await sequelize.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`);
+    console.log('Database created!');
+
+    const sequelizeDB = new Sequelize(
+      process.env.DB_NAME!,
+      process.env.DB_USER!,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+      }
+    )
+    
+    await sequelizeDB.authenticate();
+    console.log('Connected to the database successfully!');
+    
+  } catch (error) {
+    console.error('Database creation error:', error);
+  }
+};
+
+startDatabase();
 
 export default sequelize;
